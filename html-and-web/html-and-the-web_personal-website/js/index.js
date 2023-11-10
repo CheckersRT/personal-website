ddf87b5570906bd245eef6043c8af64907e65835
Tom Dent <tomdent991@hotmail.com>
2023-11-10 09:41:01 +0100

    knob snaps to middle

--- html-and-web/html-and-the-web_personal-website/js/index.js
+++ html-and-web/html-and-the-web_personal-website/js/index.js
@@ -1,66 +1,60 @@
+const meter = document.querySelector('[data-js="meter"]');
+const knob = document.querySelector('[data-js="knob"]');
 
-const meter = document.querySelector('[data-js="meter"]')
+let isMouseDown;
+let startingPosition;
+let startingOffset;
 
-
-document.addEventListener("mousedown", (event) => {
-    
-    return event.clientY;
-})
-
-console.log(startingY);
+knob.addEventListener("mousedown", (event) => {
+  isMouseDown = true;
+  startingPosition = (window.innerHeight - event.clientY) / window.innerHeight;
+  console.log(startingPosition);
+  startingOffset = 1 - startingPosition;
+  console.log(startingOffset);
+});
 
 document.addEventListener("mouseup", (event) => {
-    const endY = event.clientY;
-    console.log(endY);
-})
+  isMouseDown = false;
+  startingPosition = (window.innerHeight - event.clientY) / window.innerHeight;
+});
+
+document.addEventListener("mousemove", (event) => {
+  const rotateAmount = (window.innerHeight - (event.clientY) / window.innerHeight) + startingOffset;
+
+  if (isMouseDown) {
+    // console.log(event.clientY);
+    console.log(rotateAmount);
+    knob.style.transform = `rotate(${rotateAmount}turn)`;
+
+  } return;
+});
 
 // function findEndY() {
-    
+
 //     return endY
 //     }
-
 
 // const endY = findEndY()
 // console.log(endY);
 
-
-
-
-
-
-
-
-
-
+// return event.clientY;
 
 // const amountMoved = endY - startingY
 // console.log(amountMoved)
 
 // meter.style.height = `${(800 - event.clientY)/3}px`
 
-
-
 // document.addEventListener("mouseup", (event) => {
 
 // })
-    
 
-    // console.log(event)
-
-
-
-
-
+// console.log(event)
 
 // function changeMeterLevel(e) {
-    
+
 // }
-
-
-
 
 // y value on click
 // increase y value (px) = height in %
 
 // new MouseEvent("mousedown", screenY)
-
