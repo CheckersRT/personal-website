adccbd06a554add41a6de0d072261901e4ed6063
Tom Dent <tomdent991@hotmail.com>
2023-11-12 16:28:59 +0100

    knob position saved

--- html-and-web/html-and-the-web_personal-website/js/index.js
+++ html-and-web/html-and-the-web_personal-website/js/index.js
@@ -4,58 +4,50 @@ const knob = document.querySelector('[data-js="knob"]');
 let isMouseDown;
 let startingPosition;
 let startingOffset;
+let lastPosition = 1;
+let rotatePercentage; 
+let rotateAmount;
+
+// startingPosition = 1 or last position
+
 
 knob.addEventListener("mousedown", (event) => {
   isMouseDown = true;
-  // startingPosition = (window.innerHeight - event.clientY) / window.innerHeight;
-  console.log(startingPosition);
-  startingOffset = 1 - startingPosition;
-  console.log(startingOffset);
-});
 
-document.addEventListener("mouseup", (event) => {
-  isMouseDown = false;
   startingPosition = (window.innerHeight - event.clientY) / window.innerHeight;
-  console.log(startingPosition);
+
 });
 
 document.addEventListener("mousemove", (event) => {
-  const rotateAmount = (window.innerHeight - (event.clientY) / window.innerHeight) + startingOffset;
-
+  
   if (isMouseDown) {
-    // console.log(event.clientY);
-    console.log(rotateAmount);
-    knob.style.transform = `rotate(${rotateAmount}turn)`;
+
+  console.log(startingPosition);
+
+  movePosition = (window.innerHeight - event.clientY) / window.innerHeight;
+  console.log(`movePosition is: ${movePosition}`);
+  console.log(`lastPosition is: ${lastPosition}`);
+
+  if(lastPosition){
+    startingOffset = lastPosition -  startingPosition;
+    console.log(`startingOffset is: ${startingOffset}`);
+
+  } else {startingOffset = 1 - startingPosition;
+  }
+
+  rotateAmount = movePosition + startingOffset;
+
+  knob.style.transform = `rotate(${rotateAmount}turn)`;
+  console.log(`rotation amount is: ${knob.style.transform}`);
 
   } return;
 });
 
-// function findEndY() {
+document.addEventListener("mouseup", (event) => {
+  isMouseDown = false;
+  lastPosition = rotateAmount;
+  console.log(lastPosition);
+});
 
-//     return endY
-//     }
 
-// const endY = findEndY()
-// console.log(endY);
 
-// return event.clientY;
-
-// const amountMoved = endY - startingY
-// console.log(amountMoved)
-
-// meter.style.height = `${(800 - event.clientY)/3}px`
-
-// document.addEventListener("mouseup", (event) => {
-
-// })
-
-// console.log(event)
-
-// function changeMeterLevel(e) {
-
-// }
-
-// y value on click
-// increase y value (px) = height in %
-
-// new MouseEvent("mousedown", screenY)
